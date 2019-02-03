ccab5e3ef78b8a445885ef916bd24cc94363b77b
gargantua <istroganovi@gmail.com>
2019-02-03 22:38:06 +0300

    refactor serializers

--- app/serializers/conversion_serializer.rb
+++ app/serializers/conversion_serializer.rb
@@ -1,5 +1,5 @@
 class ConversionSerializer < ActiveModel::Serializer
-  attributes :id, :amount, :created_at
+  attributes :id, :amount, :result, :created_at
 
   belongs_to :initial_currency
   belongs_to :final_currency
